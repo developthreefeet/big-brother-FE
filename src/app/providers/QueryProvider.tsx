672cea84a14ac6dcc8e9851378b5502ddceae3ab
MinsuKim <minsu020302@gmail.com>
2024-07-07 22:00:46 +0900

    Feat : tanstack query dev-tool 설정

--- src/app/providers/QueryProvider.tsx
+++ src/app/providers/QueryProvider.tsx
@@ -1,12 +1,16 @@
 'use client';
 
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 function QueryProvider({ children }: { children: React.ReactNode }) {
   const queryClient = new QueryClient();
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
