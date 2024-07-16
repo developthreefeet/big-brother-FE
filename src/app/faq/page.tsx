7c86990ee4c7ff49164c384024e4b4b75d2a52ad
xxxjinn <tlsguswls319@naver.com>
2024-07-16 17:31:50 +0900

    style: faq 페이지 퍼블리싱

---
+++ src/app/faq/page.tsx
@@ -0,0 +1,44 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/shared/ui/ui/accordion';
+import SelectComponent from '@/widgets/SelectComponent';
+import React from 'react';
+
+const page = () => {
+  const organizationItems = ['총학', '단과대', '학과'];
+  return (
+    <div className="flex flex-col space-y-3">
+      <h1 className="font-bold text-2xl">FAQ</h1>
+      <div className="flex flex-col space-y-1">
+        <p>구분을 선택해주세요.</p>
+        <SelectComponent items={organizationItems} />
+      </div>
+      <Accordion type="single" collapsible className="w-full">
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-2">
+          <AccordionTrigger>Is it styled?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It comes with default styles that matches the other
+            components&apos; aesthetic.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-3">
+          <AccordionTrigger>Is it animated?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It's animated by default, but you can disable it if you prefer.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </div>
+  );
+};
+
+export default page;
