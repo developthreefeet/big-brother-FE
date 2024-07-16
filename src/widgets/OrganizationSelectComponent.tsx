84b0823e2c772e08f8171b84ea149efcf4bd2e25
xxxjinn <tlsguswls319@naver.com>
2024-07-16 20:54:06 +0900

    style: 총학 -> 총학생회로 워딩 변경

--- src/widgets/OrganizationSelectComponent.tsx
+++ src/widgets/OrganizationSelectComponent.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import SelectComponent from './SelectComponent';
 
 const OrganizationTitleSelectComponent = () => {
-  const organizationItems = ['총학', '단과대', '학과'];
+  const organizationItems = ['총학생회', '단과대', '학과'];
   return (
     <div className="flex flex-col space-y-1">
       <p>구분을 선택해주세요.</p>
