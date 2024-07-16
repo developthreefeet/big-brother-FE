9022f8039f3946601aee0bd6aaa6e86c6e30d63f
HyunJin <102955516+xxxjinn@users.noreply.github.com>
2024-07-16 16:49:14 +0900

    공통 컴포넌트 > 푸터 구현 (#25)
    * feature: 푸터 구현
    
    * refactor: layout.tsx 불필요한 콘솔 삭제

--- src/widgets/Footer.tsx
+++ src/widgets/Footer.tsx
@@ -1,5 +1,11 @@
 const Footer = () => {
-  return <div className="h-[90px] mt-auto bg-blue-600 text-white">Footer</div>;
+  return (
+    <div className="h-[90px] mt-auto bg-gray-100 text-white p-5 ">
+      <div className="flex flex-col space-y-2">
+        <p className="text-sm ml-0.5">ⓒ Big Brother. All rights reserved.</p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
