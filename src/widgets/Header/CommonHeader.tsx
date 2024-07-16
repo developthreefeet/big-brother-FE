765528655b0a40200c2a70fd4af1b6077938f94f
xxxjinn <tlsguswls319@naver.com>
2024-07-16 16:17:07 +0900

    feature: commonHeader 이전버튼 구현

--- src/widgets/Header/CommonHeader.tsx
+++ src/widgets/Header/CommonHeader.tsx
@@ -7,7 +7,9 @@ import {
   TooltipContent,
   Tooltip,
 } from '@/shared/ui/ui/tooltip';
+import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 import { FaUserLarge } from 'react-icons/fa6';
 import { IoIosArrowBack } from 'react-icons/io';
 
@@ -34,30 +36,33 @@ const CommonHeader = () => {
         return ' ';
     }
   };
+
+  const router = useRouter();
+
+  const handleGoBack = () => {
+    router.back();
+  };
+
   return (
     <div className="h-[50px] bg-white top-0 border-y sticky z-10 flex justify-between items-center px-2">
-      <Button variant="link">
-        {
-          <IoIosArrowBack
-            size="20"
-            className="text-gray-200 hover:text-blue-500"
-          />
-        }
+      <Button variant="link" onClick={handleGoBack}>
+        <IoIosArrowBack
+          size="20"
+          className="text-gray-200 hover:text-blue-500"
+        />
       </Button>
       <p className="text-lg font-bold text-blue-500">{getPageName()}</p>
       <TooltipProvider>
         <Tooltip>
           <TooltipTrigger asChild>
-            <Button variant="link">
-              {
-                <FaUserLarge
-                  size="16"
-                  className="text-gray-200 hover:opacity-50"
-                />
-              }
-            </Button>
+            <Link href="/profile">
+              <FaUserLarge
+                size="16"
+                className="text-gray-200 hover:opacity-50 mr-3"
+              />
+            </Link>
           </TooltipTrigger>
-          <TooltipContent className="relative -ml-5">
+          <TooltipContent className="relative -ml-10">
             <p>마이페이지</p>
           </TooltipContent>
         </Tooltip>
