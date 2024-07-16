d9a8de1e7bc4e74e519a58ca9486332106133132
xxxjinn <tlsguswls319@naver.com>
2024-07-16 14:31:24 +0900

    style: icon component 수정

--- src/views/main/IconComponent.tsx
+++ src/views/main/IconComponent.tsx
@@ -1,17 +1,18 @@
 import { Button } from '@/shared/ui/ui/button';
+import { IconType } from 'react-icons';
 
 interface IconComponentProps {
-  icon?: string;
+  icon: IconType;
   text: string;
 }
 
-const IconComponent = ({ icon, text }: IconComponentProps) => {
+const IconComponent = ({ icon: Icon, text }: IconComponentProps) => {
   return (
     <Button
       variant="link"
-      className="h-[100px] flex flex-col justify-center items-center space-y-2 hover:opacity-50 hover:no-underline text-black"
+      className="h-[100px] flex flex-col justify-center items-center space-y-3 hover:opacity-50 hover:no-underline text-black"
     >
-      <div className="w-10 h-10 bg-gray-100 border rounded-full"></div>
+      <Icon size="24" />
       <p className="text-sm">{text}</p>
     </Button>
   );
