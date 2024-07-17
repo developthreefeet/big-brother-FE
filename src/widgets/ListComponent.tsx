2838a14f79c3f37694a37ede9cedeea210f2af61
xxxjinn <tlsguswls319@naver.com>
2024-07-17 20:36:40 +0900

    feature: ListComponent (dynamic routing) 가능하도록 Link 태그로 변경

--- src/widgets/ListComponent.tsx
+++ src/widgets/ListComponent.tsx
@@ -1,16 +1,23 @@
-import { formatDate, getTitle } from '@/shared/lib/utils';
+'use client';
+
+import { formatDate, getId, getTitle } from '@/shared/lib/utils';
 import { ListComponentProps } from '@/shared/types/type';
 import DateText from './DateText';
+import Link from 'next/link';
 
 const ListComponent = ({ list }: ListComponentProps) => {
+  const { pathname } = window.location;
+
   return (
     <div className="flex flex-col space-y-2 ">
       {list.map((item, index) => (
         <div key={index} className=" border-b py-3">
-          <div className="flex flex-col space-y-1 hover:opacity-50 cursor-pointer">
-            <p className="text-sm font-bold">{getTitle(item)}</p>
-            <DateText date={formatDate(item.create_at)} />
-          </div>
+          <Link href={`${pathname}/${getId(item)}`}>
+            <div className="flex flex-col space-y-1 hover:opacity-50 cursor-pointer">
+              <p className="text-sm font-bold">{getTitle(item)}</p>
+              <DateText date={formatDate(item.create_at)} />
+            </div>
+          </Link>
         </div>
       ))}
     </div>
