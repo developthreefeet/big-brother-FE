7dcd70858a6d502c685951bb38a37de61f66c1e5
xxxjinn <tlsguswls319@naver.com>
2024-07-17 20:36:02 +0900

    style: 공지사항, 행사 detail page를 위한 commonDetailComponent 파일 생성 - 퍼블리싱

---
+++ src/views/CommonDetailComponent.tsx
@@ -0,0 +1,32 @@
+import BackToListButton from '@/widgets/BackToListButton';
+import DateText from '@/widgets/DateText';
+import Title from '@/widgets/Title';
+
+const CommonDetailComponent = () => {
+  return (
+    <div className="flex flex-col space-y-3">
+      <div className="flex flex-col space-y-1">
+        <Title text="제목이 존나 길면 어떻게 되는지 보자 디자인이" />
+        <div className="flex space-x-3 text-xs items-center text-gray-200">
+          <div className="flex space-x-1 items-center">
+            <p>생성일 | </p>
+            <DateText date="2024/07/17" />
+          </div>
+          <div className="flex space-x-1 items-center">
+            <p>수정일 | </p>
+            <DateText date="2024/07/17" />
+          </div>
+        </div>
+      </div>
+      <hr />
+      <div className="p-2 text-md">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi obcaecati
+      </div>
+      <div className="flex justify-end">
+        <BackToListButton />
+      </div>
+    </div>
+  );
+};
+
+export default CommonDetailComponent;
