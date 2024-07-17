009edcd9c5f08617c95e0670ef0bb24b64f4a12a
xxxjinn <tlsguswls319@naver.com>
2024-07-17 19:32:50 +0900

    refactor: 날짜 text component로 따로 생성

--- src/widgets/ListComponent.tsx
+++ src/widgets/ListComponent.tsx
@@ -1,5 +1,6 @@
 import { formatDate, getTitle } from '@/shared/lib/utils';
 import { ListComponentProps } from '@/shared/types/type';
+import DateText from './DateText';
 
 const ListComponent = ({ list }: ListComponentProps) => {
   return (
@@ -8,9 +9,7 @@ const ListComponent = ({ list }: ListComponentProps) => {
         <div key={index} className=" border-b py-3">
           <div className="flex flex-col space-y-1 hover:opacity-50 cursor-pointer">
             <p className="text-sm font-bold">{getTitle(item)}</p>
-            <p className="text-xs text-gray-200">
-              {formatDate(item.create_at)}
-            </p>
+            <DateText date={formatDate(item.create_at)} />
           </div>
         </div>
       ))}
