3da46977f0d9a11ec6d37978755254e466ce6e44
xxxjinn <tlsguswls319@naver.com>
2024-07-18 17:21:17 +0900

    feature: pdfViewer 컴포넌트 생성

---
+++ src/widgets/PdfViewer.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { useState } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
+import 'react-pdf/dist/Page/TextLayer.css';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
+
+const PdfViewer = ({ pdf }: { pdf: string }) => {
+  const [numPages, setNumPages] = useState<number>();
+
+  function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
+    setNumPages(numPages);
+  }
+
+  return (
+    <>
+      <Document file={pdf} onLoadSuccess={onDocumentLoadSuccess}>
+        {Array.from(new Array(numPages), (_, index) => (
+          <div className="mb-1">
+            <Page
+              width={300}
+              key={index}
+              pageNumber={index + 1}
+              renderAnnotationLayer={false}
+            />
+          </div>
+        ))}
+      </Document>
+    </>
+  );
+};
+
+export default PdfViewer;
