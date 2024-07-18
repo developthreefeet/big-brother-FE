2b8a5fa4d18f67604a2e4a0caa8e0fcff0c65d20
xxxjinn <tlsguswls319@naver.com>
2024-07-18 17:21:34 +0900

    style: 회의록 detail 페이지 퍼블리싱 (하드코딩)

---
+++ src/app/proceeding/[id]/page.tsx
@@ -0,0 +1,22 @@
+import BackToListButton from '@/widgets/BackToListButton';
+import DateText from '@/widgets/DateText';
+import PdfViewer from '@/widgets/PdfViewer';
+import Title from '@/widgets/Title';
+
+const page = () => {
+  return (
+    <div className="flex flex-col space-y-3">
+      <div className="flex flex-col space-y-1">
+        <Title text="회의록1" />
+        <DateText date="2024/07/18" />
+      </div>
+      <hr />
+      <PdfViewer pdf="/static/test.pdf" />
+      <div className="flex justify-end">
+        <BackToListButton />
+      </div>
+    </div>
+  );
+};
+
+export default page;
