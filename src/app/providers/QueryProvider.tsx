7dbc4a2cfc7f9292b47c8602baaf63daf196a2f4
xxxjinn <tlsguswls319@naver.com>
2024-07-23 03:27:48 +0900

    build에러 보려고 수정

--- src/app/providers/QueryProvider.tsx
+++ src/app/providers/QueryProvider.tsx
@@ -20,7 +20,7 @@ function QueryProvider({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      {/*<ReactQueryDevtools initialIsOpen={false} />*/}
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
