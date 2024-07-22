b73c1e1aaae575a1d89b30bd71c6af486e7df8f8
HyunJin <102955516+xxxjinn@users.noreply.github.com>
2024-07-23 03:58:16 +0900

    Comment/publishing (#47)
    * setting: install shadcn textarea
    
    * feature: 댓글 스타일링
    
    * delete: 불필요한 파일 삭제

---
+++ src/widgets/CommentList.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const CommentList = () => {
+  const commentList = [
+    {
+      id: 1,
+      user_name: '유저1',
+      comment: '댓글컨텐츠',
+    },
+    {
+      id: 2,
+      user_name: '유저2',
+      comment: '댓글컨텐츠 어쩌고저쩌고 힘들다',
+    },
+  ];
+  return (
+    <div className="flex flex-col space-y-3">
+      <p className="font-bold text-md">댓글</p>
+      {commentList.map((item, index) => (
+        <div key={index} className="flex flex-col space-y-3">
+          <div className="flex flex-col space-y-2">
+            <p className="font-bold text-sm">{item.user_name}</p>
+            <p className="text-gray-300 text-sm">{item.comment}</p>
+          </div>
+          <hr />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default CommentList;
