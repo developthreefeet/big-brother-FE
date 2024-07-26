b72fea6176b40eb2b9e3b18be5af745eb35fb7ef
xxxjinn <tlsguswls319@naver.com>
2024-07-26 17:55:12 +0900

    feature: 사용자가 이메일 인증을 받지 않고 /join으로 이동한 경우, /join/email로 페이지 이동

--- src/app/join/page.tsx
+++ src/app/join/page.tsx
@@ -1,6 +1,20 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 import JoinForm from '@/features/account/ui/JoinForm';
+import { useEmailStore } from '@/features/account/model/useEmailStore';
 
 const Page = () => {
+  const router = useRouter();
+  const { verificationComplete } = useEmailStore();
+
+  useEffect(() => {
+    if (!verificationComplete) {
+      router.push('/join/email');
+    }
+  }, [verificationComplete]);
+
   return (
     <>
       <h3 className="font-bold">정보를 입력해 주세요.</h3>
