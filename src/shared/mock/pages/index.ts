ca0d0bbe1071071b03f1d83da5ffd9b3eb20a241
xxxjinn <tlsguswls319@naver.com>
2024-07-27 16:23:41 +0900

    feature: 메인 페이지에서 각 페이지로 이동 시, 총학생회가 기본으로 나오도록 라우팅 수정

--- src/shared/mock/pages/index.ts
+++ src/shared/mock/pages/index.ts
@@ -8,10 +8,10 @@ import {
 } from 'react-icons/fc';
 
 export const pages = [
-  { icon: FcList, text: '행사', href: '/event' },
-  { icon: FcRules, text: '회의록', href: '/proceeding' },
-  { icon: FcDepartment, text: '학칙/회칙', href: '/rule' },
-  { icon: FcCustomerSupport, text: 'FAQ', href: '/faq' },
+  { icon: FcList, text: '행사', href: '/event/studentCouncil' },
+  { icon: FcRules, text: '회의록', href: '/proceeding/studentCouncil' },
+  { icon: FcDepartment, text: '학칙/회칙', href: '/rule/studentCouncil' },
+  { icon: FcCustomerSupport, text: 'FAQ', href: '/faq/studentCouncil' },
   { icon: FcCurrencyExchange, text: '입/출금 내역', href: '/transaction' },
   { icon: FcBusinessman, text: '마이페이지', href: '/mypage' },
 ];
