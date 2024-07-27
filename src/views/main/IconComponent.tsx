97cfe1fd4438e2030ff5780b528d0d7f8cb67f96
xxxjinn <tlsguswls319@naver.com>
2024-07-27 15:47:05 +0900

    refactor: IconComponent 자체에 Link 포함시키는 걸로 수정

--- src/views/main/IconComponent.tsx
+++ src/views/main/IconComponent.tsx
@@ -1,20 +1,24 @@
 import { Button } from '@/shared/ui/ui/button';
+import Link from 'next/link';
 import { IconType } from 'react-icons';
 
 interface IconComponentProps {
   icon: IconType;
   text: string;
+  href: string;
 }
 
-const IconComponent = ({ icon: Icon, text }: IconComponentProps) => {
+const IconComponent = ({ icon: Icon, text, href }: IconComponentProps) => {
   return (
-    <Button
-      variant="link"
-      className="w-[100px] h-[100px] flex flex-col space-y-3 hover:opacity-50 hover:no-underline text-black"
-    >
-      <Icon size="24" />
-      <p className="text-sm">{text}</p>
-    </Button>
+    <Link href={href} className="flex items-center flex-col justify-center">
+      <Button
+        variant="link"
+        className="w-[100px] h-[100px] flex flex-col space-y-3 hover:opacity-50 hover:no-underline text-black"
+      >
+        <Icon size="24" />
+        <p className="text-sm">{text}</p>
+      </Button>
+    </Link>
   );
 };
 
