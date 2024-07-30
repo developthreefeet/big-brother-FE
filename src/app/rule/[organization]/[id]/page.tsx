1581462cfdad644cc03a3697db0bfbbd766ab2de
xxxjinn <tlsguswls319@naver.com>
2024-07-31 00:43:43 +0900

    feature: detail 페이지 (회의록/행사) 로직 구현

--- src/app/rule/[organization]/[id]/page.tsx
+++ src/app/rule/[organization]/[id]/page.tsx
@@ -1,7 +1,17 @@
+'use client';
+
+import useDetail from '@/shared/hooks/useDetail';
+import { ruleItems } from '@/shared/mock/contentList';
 import PdfViewerDetailComponent from '@/widgets/PdfViewerDetailComponent';
 
 const page = () => {
-  return <PdfViewerDetailComponent />;
+  const ruleItem = useDetail(ruleItems);
+
+  if (!ruleItem) {
+    return null;
+  }
+
+  return <PdfViewerDetailComponent item={ruleItem} />;
 };
 
 export default page;
