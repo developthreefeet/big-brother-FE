960e9ca55aaaedd239db8be5edd99be19f85d176
xxxjinn <tlsguswls319@naver.com>
2024-08-14 01:30:47 +0900

    feat: instance 하나만 사용하도록 구현 + interceptor 일부 구현

--- src/shared/api/instance.ts
+++ src/shared/api/instance.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { deleteCookie, getCookie } from 'cookies-next';
 
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
@@ -11,9 +12,12 @@ export const instance = axios.create({
   withCredentials: true,
 });
 
-const reqInterceptor = instance.interceptors.request.use(
+instance.interceptors.request.use(
   (config) => {
-    console.log('Request Interceptor', config);
+    const accessToken = getCookie('accessToken');
+    if (accessToken) {
+      config.headers['Authorization'] = `Bearer ${accessToken}`;
+    }
     return config;
   },
   (error) => {
@@ -21,12 +25,42 @@ const reqInterceptor = instance.interceptors.request.use(
   },
 );
 
-const resInterceptor = instance.interceptors.response.use(
+const handleUnauthorized = () => {
+  deleteCookie('accessToken');
+  deleteCookie('refreshToken');
+  alert('세션이 만료되었습니다. 다시 로그인해주세요.');
+  window.location.replace('/login');
+};
+
+instance.interceptors.response.use(
   (response) => {
-    console.log('Response Interceptor', response);
     return response;
   },
   (error) => {
+    //에러코드 임시로 달아둠
+    if (error.response?.status === 401) {
+      if (error.response?.data && 'code' in error.response.data) {
+        // 리프레시 토큰이 유효하지 않을 때 (에러코드 임시)
+        if (error.response.data.code === 1001) {
+          handleUnauthorized();
+        }
+      } else {
+        const accessToken = getCookie('accessToken');
+        if (accessToken) {
+          //refresh 토큰 재요청하는 api
+          /*const res = await REFRESH_API.refreshToken({
+            accessToken: accessToken,
+            refreshToken: getCookie('refreshToken') as string,
+          });
+          setCookie('accessToken', res.data.accessToken);
+          setCookie('refreshToken', res.data.refreshToken);*/
+        } else {
+          deleteCookie('accessToken');
+          deleteCookie('refreshToken');
+          window.location.replace('/login');
+        }
+      }
+    }
     return Promise.reject(error);
   },
 );
