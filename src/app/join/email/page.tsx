76a5447643442bef28c64633b79fee65671c195b
xxxjinn <tlsguswls319@naver.com>
2024-08-14 04:00:57 +0900

    feat: 로그인 상태에서 회원가입 url로 바로 이동 시 메인으로 리다이렉션

--- src/app/join/email/page.tsx
+++ src/app/join/email/page.tsx
@@ -1,8 +1,29 @@
 'use client';
 
+import AlreadyLoggedInNotice from '@/features/account/ui/AlreadyLoggedInNotice';
 import JoinEmailForm from '@/features/account/ui/JoinEmailForm';
+import { getCookie } from 'cookies-next';
+import { useRouter } from 'next/navigation';
+import { useState, useEffect } from 'react';
 
 const page = () => {
+  const router = useRouter();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const accessToken = getCookie('accessToken');
+
+    if (accessToken) {
+      router.push('/main');
+    } else {
+      setLoading(false);
+    }
+  }, [router]);
+
+  if (loading) {
+    return <AlreadyLoggedInNotice />;
+  }
+
   return (
     <>
       <h3 className="font-bold">학교 이메일 인증</h3>
