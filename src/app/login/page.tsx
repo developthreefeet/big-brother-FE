49b996a054b223db7914e0c23c98b523a6184ea9
xxxjinn <tlsguswls319@naver.com>
2024-08-14 03:48:54 +0900

    feat:로그인 된 상태에서 로그인 페이지로 이동한 경우 메인으로 리다이렉션

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,17 +1,35 @@
 'use client';
 
 import { useJoinEmailStore } from '@/features/account/model/useJoinEmailStore';
+import AlreadyLoggedInNotice from '@/features/account/ui/AlreadyLoggedInNotice';
 import ChangePassword from '@/features/account/ui/ChangePassword';
 import GoToJoin from '@/features/account/ui/GoToJoin';
 import LoginForm from '@/features/account/ui/LoginForm';
+import { getCookie } from 'cookies-next';
 import Image from 'next/image';
-import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import { BounceLoader } from 'react-spinners';
 
-const page = () => {
+const Page = () => {
   const { resetVerificationComplete } = useJoinEmailStore();
+  const router = useRouter();
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     resetVerificationComplete();
-  }, []);
+    const accessToken = getCookie('accessToken');
+
+    if (accessToken) {
+      router.push('/main');
+    } else {
+      setLoading(false);
+    }
+  }, [router]);
+
+  if (loading) {
+    return <AlreadyLoggedInNotice />;
+  }
 
   return (
     <>
@@ -34,4 +52,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Page;
