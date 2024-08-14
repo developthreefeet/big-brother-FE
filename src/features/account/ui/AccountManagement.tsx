c28fdaa4fb3dea863fad3b0db84e354917621968
xxxjinn <tlsguswls319@naver.com>
2024-08-14 16:48:13 +0900

    feat: mypage 계정 관련 - 비밀번호 변경 버튼 추가

--- src/features/account/ui/AccountManagement.tsx
+++ src/features/account/ui/AccountManagement.tsx
@@ -1,32 +1,29 @@
+'use client';
+
 import { Button } from '@/shared/ui/ui/button';
+import { useRouter } from 'next/navigation';
 
 const AccountManagement = () => {
+  const router = useRouter();
   return (
     <ul>
-      {/* <li>
+      <li>
         <Button
           variant="link"
           className="text-black p-0 hover:no-underline hover:opacity-50"
+          onClick={() => router.push('/changePw')}
         >
-          {`알림 설정`}
+          비밀번호 변경
         </Button>
-      </li>*/}
+      </li>
       <li>
         <Button
           variant="link"
           className="text-black p-0 hover:no-underline hover:opacity-50"
         >
-          {`로그아웃`}
+          로그아웃
         </Button>
       </li>
-      {/*<li>
-        <Button
-          variant="link"
-          className="text-black p-0 hover:no-underline hover:opacity-50"
-        >
-          {`회원 탈퇴`}
-        </Button>
-      </li>*/}
     </ul>
   );
 };
