969e499d6dccd278840a4d2ab8cf5e9be1ee5d81
xxxjinn <tlsguswls319@naver.com>
2024-08-14 16:37:59 +0900

    refactor: 불필요한 코드 삭제

--- src/app/changePw/page.tsx
+++ src/app/changePw/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { useChangePwEmail } from '@/features/account/model/useChangePwEmail';
 import { useChangePwEmailStore } from '@/features/account/model/useChangePwEmailStore';
 import EmailForm from '@/features/account/ui/EmailForm';
 import NewPasswordForm from '@/features/account/ui/NewPasswordForm';
