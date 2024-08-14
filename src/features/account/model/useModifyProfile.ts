0fc6fc55a0dd35ae5f382333066c696c02ddec3a
xxxjinn <tlsguswls319@naver.com>
2024-08-15 04:03:01 +0900

    refactor: 불필요한 코드 삭제

--- src/features/account/model/useModifyProfile.ts
+++ src/features/account/model/useModifyProfile.ts
@@ -6,7 +6,6 @@ import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 import { usePatchModifyProfile } from '../api/queries';
 import { toast } from '@/shared/ui/ui/use-toast';
-import { useUserNameStore } from './useUserNameStore';
 
 export const useModifyProfile = () => {
   const userNameRegex = /^[가-힣]+$/;
@@ -18,8 +17,6 @@ export const useModifyProfile = () => {
       .max(15, { message: '15글자 이하로 입력해 주세요.' })
       .regex(userNameRegex, { message: '한글 조합만 사용 가능합니다.' }),
   });
-
-  const { userName } = useUserNameStore();
 
   const form = useForm({
     resolver: zodResolver(userSchema),
