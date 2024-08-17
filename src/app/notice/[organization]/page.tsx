dfcd206c189f20129c3dc8fd15a0bf0e941f93d4
xxxjinn <tlsguswls319@naver.com>
2024-08-18 02:08:36 +0900

    refactor: 목록 페이지 무한스크롤 버그 관련 고침

--- src/app/notice/[organization]/page.tsx
+++ src/app/notice/[organization]/page.tsx
@@ -2,10 +2,19 @@
 
 import useNotice from '@/features/contentList/model/useNotice';
 import ListLayoutComponent from '@/features/contentList/ui/ListLayoutComponent';
+import { useQueryClient } from '@tanstack/react-query';
+import { useEffect } from 'react';
 
 const page = () => {
+  const queryClient = useQueryClient();
   const { data, fetchNextPage, hasNextPage, isFetchingNextPage, isLoading } =
     useNotice();
+
+  useEffect(() => {
+    return () => {
+      queryClient.resetQueries();
+    };
+  }, [queryClient]);
 
   const allContent = data?.pages.flatMap((page) => page.content);
   return (
