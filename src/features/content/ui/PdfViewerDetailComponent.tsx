f67e3665f54bd40c7de079bf7866e9c596f1436a
xxxjinn <tlsguswls319@naver.com>
2024-08-19 19:23:04 +0900

    feat: pdf detail 페이지 생성일, 수정일 관련 수정

--- src/features/content/ui/PdfViewerDetailComponent.tsx
+++ src/features/content/ui/PdfViewerDetailComponent.tsx
@@ -1,21 +1,36 @@
+'use client';
+
 import { formatDate } from '@/shared/lib/utils';
 import BackToListButton from '@/features/content/ui/BackToListButton';
 import DateText from '@/widgets/DateText';
 import PdfViewer from './PdfViewer';
 import { PdfDetailItem } from '../api/types';
 import Title from '@/widgets/Title';
+import { usePathname } from 'next/navigation';
 
 const PdfViewerDetailComponent = ({
   item,
 }: {
   item: PdfDetailItem | undefined;
 }) => {
+  const pathname = usePathname();
   if (item) {
     return (
       <div className="flex flex-col space-y-3">
         <div className="flex flex-col space-y-1">
           <Title text={item.title} />
-          <DateText date={formatDate(item.createAt)} />
+          <div className="flex space-x-3 text-xs items-center text-gray-200">
+            <div className="flex space-x-1 items-center">
+              <p>생성일 | </p>
+              <DateText date={formatDate(item.createAt)} />
+            </div>
+            {pathname.includes('proceeding') && (
+              <div className="flex space-x-1 items-center">
+                <p>수정일 | </p>
+                <DateText date={formatDate(item.updateAt)} />
+              </div>
+            )}
+          </div>
         </div>
         <hr />
         <PdfViewer pdf={item.fileInfo[0].url} />
