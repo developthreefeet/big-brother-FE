1078eb5dba3689dd2d1ac7527e52cfd4280d5148
xxxjinn <tlsguswls319@naver.com>
2024-09-09 16:32:56 +0900

    feat: 총 입/출금액 skeleton 적용

--- src/views/transaction/TotalAmount.tsx
+++ src/views/transaction/TotalAmount.tsx
@@ -1,5 +1,6 @@
 import { GetTransactionDetailResData } from '@/features/content/api/types';
 import { calculateTotals } from '@/shared/lib/utils';
+import { Skeleton } from '@/shared/ui/ui/skeleton';
 
 const TotalAmount = ({
   items,
@@ -14,7 +15,11 @@ const TotalAmount = ({
     <div className="flex flex-col space-y-3">
       <p className="text-xl font-bold">총 입/출금액</p>
       {isLoading ? (
-        <div>로딩중...</div>
+        <div className="flex flex-col gap-3">
+          {Array.from({ length: 2 }).map((_, index) => (
+            <Skeleton key={index} className="h-5 w-24" />
+          ))}
+        </div>
       ) : (
         <div className="flex flex-col space-y-1 font-bold">
           <p>- {totalWithdraw.toLocaleString()} 원</p>
