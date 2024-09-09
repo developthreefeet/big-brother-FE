acb7a4f6943163bf99da78c5cd227cebad5cf7b4
xxxjinn <tlsguswls319@naver.com>
2024-09-09 16:27:25 +0900

    feat: faq 페이지 skeleton 적용

--- src/app/faq/[organization]/page.tsx
+++ src/app/faq/[organization]/page.tsx
@@ -6,6 +6,7 @@ import useOrganizationRouter from '@/features/contentList/model/useOrganizationR
 import AccordionComponent from '@/features/content/ui/AccordionComponent';
 import { usePathname } from 'next/navigation';
 import useCommonDetail from '@/features/content/model/useCommonDetail';
+import { Skeleton } from '@/shared/ui/ui/skeleton';
 
 const Page = () => {
   useOrganizationRouter();
@@ -14,14 +15,22 @@ const Page = () => {
 
   const { returnFaqDetailItem } = useCommonDetail();
 
-  let faqData = returnFaqDetailItem(organization);
-  const faqItem = faqData?.pages.flatMap((page) => page.content);
+  const { data, isLoading } = returnFaqDetailItem(organization);
+  const faqItem = data?.pages.flatMap((page) => page.content);
 
   return (
     <div className="flex flex-col space-y-3">
       <Title text="FAQ" />
       <OrganizationTitleSelectComponent />
-      <AccordionComponent items={faqItem} />
+      {isLoading ? (
+        <div className="flex flex-col gap-6">
+          {Array.from({ length: 6 }).map((_, index) => (
+            <Skeleton key={index} className="h-7 w-80" />
+          ))}
+        </div>
+      ) : (
+        <AccordionComponent items={faqItem} />
+      )}
     </div>
   );
 };
