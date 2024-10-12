b3c6f87b13ac301d2d58979e83b59eccef94b8ad
xxxjinn <tlsguswls319@naver.com>
2024-10-12 17:50:35 +0900

    style: miniNotice slice 숫자 수정

--- src/features/contentList/ui/MiniNoticeList.tsx
+++ src/features/contentList/ui/MiniNoticeList.tsx
@@ -11,7 +11,7 @@ const MiniNoticeList = () => {
     useGetCampusNotice('일반공지');
 
   return (
-    <div className="flex flex-col w-full space-y-1">
+    <div className="flex flex-col w-full space-y-1 mt-4">
       <div className="flex flex-row items-center justify-between">
         <p className="text-xl font-bold">공지사항</p>
         <Button
@@ -21,19 +21,16 @@ const MiniNoticeList = () => {
           <Link href="/notice/general">{'바로가기 >'}</Link>
         </Button>
       </div>
-
       {isLoading && (
         <div className="flex flex-col gap-2">
           {Array.from({ length: 5 }).map((_, index) => (
-            <Skeleton key={index} className="h-5 w-80" />
+            <Skeleton key={index} className="h-5 w-160" />
           ))}
         </div>
       )}
-
       {isError && (
         <div>공지사항을 불러오지 못했습니다. 관리자에게 문의해주세요.</div>
       )}
-
       {isSuccess && data && (
         <div className="flex flex-col ml-1">
           {data.content.slice(0, 5).map((content, index) => (
@@ -42,7 +39,7 @@ const MiniNoticeList = () => {
                 variant="link"
                 className="justify-start text-left hover:no-underline hover:opacity-50 text-gray-300 pl-0 py-0 h-7"
               >
-                {truncateTitle(content.title, 29)}
+                {truncateTitle(content.title, 40)}
               </Button>
             </Link>
           ))}
